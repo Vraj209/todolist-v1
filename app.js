9b297d20b22e4f8b4a465e1c4188c06b518fabbf
Vraj209 <80612219+Vraj209@users.noreply.github.com>
2023-06-01 16:24:51 -0400

    User add item

--- app.js
+++ app.js
@@ -3,46 +3,30 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+var items = ["Buy Food","Cook Food","Eat Food"];
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday"
-
-            break;
-        case 1:
-
-            day = "Monday"
-            break;
-        case 2:
-            day = "Tuesday"
-            break;
-        case 3:
-            day = "Wednesday"
-            break;
-        case 4:
-            day = "Thursday"
-            break;
-        case 5:
-            day = "Friday"
-            break;
-        case 6:
-            day = "Saturday"
-            break;
-        default:
-            console.log("Error: current day is equal to: " + currentDay)
-            break;
+    var option = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
     }
-    
-    res.render('list', { kindOfDay: day })
+
+    var day = today.toLocaleDateString("en-US", option)
+    res.render('list', { kindOfDay: day, newListItems: items })
 })
 
+app.post('/', (req, res) => {
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/")
+    console.log(item)
+})
 
 // listen to 3000 port
 app.listen(3000, () => {
