d83c9a44dc8a372ab0abcfbecce53072ad89bd51
Vraj209 <80612219+Vraj209@users.noreply.github.com>
2023-06-03 08:27:07 -0400

    complete v1

--- app.js
+++ app.js
@@ -2,31 +2,43 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const date = require(__dirname + '/date.js')
+
 const app = express()
-var items = ["Buy Food","Cook Food","Eat Food"];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"))
 
 app.get('/', (req, res) => {
-    var today = new Date();
-
-    var option = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    }
-
-    var day = today.toLocaleDateString("en-US", option)
-    res.render('list', { kindOfDay: day, newListItems: items })
+    const day = date.getDate()
+    res.render('list', { listTitle: day, newListItems: items })
 })
 
 app.post('/', (req, res) => {
-    var item = req.body.newItem;
-    items.push(item);
-    res.redirect("/")
-    console.log(item)
+    const item = req.body.newItem;
+
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work")
+
+
+    } else {
+
+        items.push(item);
+        res.redirect("/")
+        // console.log(item)
+    }
+})
+
+app.get('/work', (req, res) => {
+    res.render('list', { listTitle: "Work List", newListItems: workItems })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
 })
 
 // listen to 3000 port
